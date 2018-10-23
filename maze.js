411e5abb2ee5cbace792020b051f96bfd118f075
Tonnika <shantiwilliams11@gmail.com>
2018-10-22 18:02:07 -0800

    exercise 2

--- maze.js
+++ maze.js
@@ -2,13 +2,33 @@
 window.onload = function (){
 	var body = document.getElementsByTagName("body");
 	console.log(body);
-	
-	document.getElementById("boundary1").addEventListener("mouseover",mouseOver);
-	//document.getElementById("boundary1").addEventListener("mouseout", mouseOut);
+	//exercise 1 
+	/*document.getElementById("boundary1").addEventListener("mouseover",mouseOver);
 		
 	function mouseOver(){
 		document.getElementById("boundary1").classList.add("youlose");
+	}*/
+	
+	
+	// exercise 2 
+	
+	var boundaries = document.querySelectorAll("div.boundary");
+	for(var i=0; i<boundaries.length; i++){
+		boundaries[i].addEventListener("mouseover",MouseOverBoundary);
+		//boundaries[i].classList.add("youlose");
+		//function MouseOverBoundary(){
+			//this.classList.add("youlose");
+		
 	}
 	
+	function MouseOverBoundary(){
+		var boundaries = document.querySelectorAll("div.boundary");
+		for (var i=0; i<boundaries.length; i++){
+			boundaries[i].classList.add("youlose");
+		}
+	}
+	
+	
+
 	
 };
