397d154b83970ec838044c202bb177b6ddcb0852
Tonnika <shantiwilliams11@gmail.com>
2018-10-22 19:00:01 -0800

    exercise 3

--- maze.js
+++ maze.js
@@ -15,20 +15,43 @@ window.onload = function (){
 	var boundaries = document.querySelectorAll("div.boundary");
 	for(var i=0; i<boundaries.length; i++){
 		boundaries[i].addEventListener("mouseover",MouseOverBoundary);
-		//boundaries[i].classList.add("youlose");
-		//function MouseOverBoundary(){
-			//this.classList.add("youlose");
-		
 	}
 	
+	/*function MouseOverBoundary(){
+		var boundaries = document.querySelectorAll("div.boundary");
+		for (var i=0; i<boundaries.length; i++){
+			boundaries[i].classList.add("youlose");
+		}
+	}*/
+	
+	//exercise 3 
+	var winner = true;
+	document.getElementById("start").addEventListener("mouseover",MouseOverS);
+	document.getElementById("end").addEventListener("mouseover",MouseOverE);	
+	
 	function MouseOverBoundary(){
+		winner=true; 
 		var boundaries = document.querySelectorAll("div.boundary");
 		for (var i=0; i<boundaries.length; i++){
 			boundaries[i].classList.add("youlose");
 		}
 	}
 	
+	function MouseOverS(){
+		winner = false;
+		var boundaries = document.querySelectorAll("div.boundary");
+		for(var i=0; i<boundaries.length; i++){
+			boundaries[i].classList.remove("youlose");
+		
+		}
+	}
 	
-
+	function MouseOverE(){
+		if(winner){
+			alert("Awwww, you lost. Try again!");
+		}else{
+			alert("You won! Awesome");
+		}
+	}
 	
 };
