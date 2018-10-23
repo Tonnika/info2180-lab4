4a1212441ea106b21f6293565e210d43174c8f81
Tonnika <shantiwilliams11@gmail.com>
2018-10-22 19:24:16 -0800

    exercise 4

--- maze.js
+++ maze.js
@@ -25,7 +25,7 @@ window.onload = function (){
 	}*/
 	
 	//exercise 3 
-	var winner = true;
+	/*var winner = true;
 	document.getElementById("start").addEventListener("mouseover",MouseOverS);
 	document.getElementById("end").addEventListener("mouseover",MouseOverE);	
 	
@@ -52,6 +52,38 @@ window.onload = function (){
 		}else{
 			alert("You won! Awesome");
 		}
+		
+	}*/
+	
+	//exercise 4 
+	var winner = true;
+	document.getElementById("start").addEventListener("click",mouseClick);
+	document.getElementById("end").addEventListener("mouseover",MouseOverE);	
+	
+	function MouseOverBoundary(){
+		winner=true; 
+		var boundaries = document.querySelectorAll("div.boundary");
+		for (var i=0; i<boundaries.length; i++){
+			boundaries[i].classList.add("youlose");
+		}
 	}
+	
+	 function mouseClick(){
+		 winner=false;
+		var boundaries = document.querySelectorAll("div.boundary");
+		for(var i=0; i<boundaries.length; i++){
+			boundaries[i].classList.remove("youlose");
+		}
+	 }
+	 
+	function MouseOverE(){
+		if(winner){
+			alert("Awwww, you lost. Try again!");
+		}else{
+			alert("You won! Awesome");
+		}
+	}
+	
+	
 	
 };
